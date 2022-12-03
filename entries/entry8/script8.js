47101772006a019ef8671c732d5331ec9e7c10e4
SizhenChen <chens656@newschool.edu>
2022-12-02 19:51:14 -0500

    commit

---
+++ entries/entry8/script8.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {alert("FLASH WARNING")})
+
+$(document).ready(function() {$("#碎屏").hide(); })
+
+$(document).ready(function() {
+    $("#message1").click(function(){$("#碎屏").show(); });})
+
+$(document).ready(function() {
+    $("#message2").click(function(){$("#碎屏").show(); });})
+
+$(document).ready(function() {
+    $("#message3").click(function(){$("#碎屏").show(); });})
